5d28a76e8d7ceb24229ad9ebcb9b2b0e68f7080b
Kathy Zhang <112294375+b27b@users.noreply.github.com>
2025-01-13 14:56:03 -0500

    fix navbar

--- app/Components/Navbar/Navbar.jsx
+++ app/Components/Navbar/Navbar.jsx
@@ -1,14 +1,17 @@
 export default function Navbar() {
   return (
-
-
-    <nav className="family-name:var(--font-reddit-mono) bg-darkcolour bg-[url('/images/backgrounds/felt-texture.png')] bg-contain text-lightcolour flex justify-between mx-12 py-6 px-16 top-6 rounded-3xl w-11/12 fixed z-50 shadow-[0_20px_5px_-15px_rgba(0,0,0,0.3)]">
-      <href>Kathy Zhang</href>
-      <div className="flex justify-between gap-4">
-        <href>works</href>
-        <href>about</href>
-        <href>contact</href>
-      </div>
-    </nav>
+    <section className=" ">
+      <nav className="family-name:var(--font-reddit-mono) text-lightcolour 
+      bg-[url('/images/backgrounds/felt-texture.png')] z-50 bg-darkcolour bg-cover bg-no-repeat 
+      flex justify-between mx-12 py-6 px-16 top-6 rounded-3xl w-11/12 fixed  
+      shadow-[0_20px_5px_-15px_rgba(0,0,0,0.3)]">
+        <href>Kathy Zhang</href>
+        <div className="flex justify-between gap-4">
+          <href>works</href>
+          <href>about</href>
+          <href>contact</href>
+        </div>
+      </nav>
+    </section>
   );
 }
